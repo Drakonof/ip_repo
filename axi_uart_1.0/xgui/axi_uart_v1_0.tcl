8f1200ec7e1ec325dbbfd201bd4277629a8c3c11
ashimko-sc <artem.shimko@syntacore.com>
2022-03-16 17:19:45 +0300

    first commit

--- axi_uart_1.0/xgui/axi_uart_v1_0.tcl
+++ axi_uart_1.0/xgui/axi_uart_v1_0.tcl
@@ -1,51 +1,33 @@
 # Definitional proc to organize widgets for parameters.
 proc init_gui { IPINST } {
   ipgui::add_param $IPINST -name "Component_Name"
+  #Adding Page
+  set Page_0 [ipgui::add_page $IPINST -name "Page 0"]
+  set C_S00_AXI_DATA_WIDTH [ipgui::add_param $IPINST -name "C_S00_AXI_DATA_WIDTH" -parent ${Page_0} -widget comboBox]
+  set_property tooltip {Width of S_AXI data bus} ${C_S00_AXI_DATA_WIDTH}
+  set C_S00_AXI_ADDR_WIDTH [ipgui::add_param $IPINST -name "C_S00_AXI_ADDR_WIDTH" -parent ${Page_0}]
+  set_property tooltip {Width of S_AXI address bus} ${C_S00_AXI_ADDR_WIDTH}
+  ipgui::add_param $IPINST -name "C_S00_AXI_BASEADDR" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "C_S00_AXI_HIGHADDR" -parent ${Page_0}
+
 
 }
 
-proc update_PARAM_VALUE.AXI_DATA_WIDTH { PARAM_VALUE.AXI_DATA_WIDTH } {
-	# Procedure called to update AXI_DATA_WIDTH when any of the dependent parameters in the arguments change
+proc update_PARAM_VALUE.C_S00_AXI_DATA_WIDTH { PARAM_VALUE.C_S00_AXI_DATA_WIDTH } {
+	# Procedure called to update C_S00_AXI_DATA_WIDTH when any of the dependent parameters in the arguments change
 }
 
-proc validate_PARAM_VALUE.AXI_DATA_WIDTH { PARAM_VALUE.AXI_DATA_WIDTH } {
-	# Procedure called to validate AXI_DATA_WIDTH
+proc validate_PARAM_VALUE.C_S00_AXI_DATA_WIDTH { PARAM_VALUE.C_S00_AXI_DATA_WIDTH } {
+	# Procedure called to validate C_S00_AXI_DATA_WIDTH
 	return true
 }
 
-proc update_PARAM_VALUE.BAUD_MAX_VALUE { PARAM_VALUE.BAUD_MAX_VALUE } {
-	# Procedure called to update BAUD_MAX_VALUE when any of the dependent parameters in the arguments change
+proc update_PARAM_VALUE.C_S00_AXI_ADDR_WIDTH { PARAM_VALUE.C_S00_AXI_ADDR_WIDTH } {
+	# Procedure called to update C_S00_AXI_ADDR_WIDTH when any of the dependent parameters in the arguments change
 }
 
-proc validate_PARAM_VALUE.BAUD_MAX_VALUE { PARAM_VALUE.BAUD_MAX_VALUE } {
-	# Procedure called to validate BAUD_MAX_VALUE
-	return true
-}
-
-proc update_PARAM_VALUE.CLK_VALUE_MHZ { PARAM_VALUE.CLK_VALUE_MHZ } {
-	# Procedure called to update CLK_VALUE_MHZ when any of the dependent parameters in the arguments change
-}
-
-proc validate_PARAM_VALUE.CLK_VALUE_MHZ { PARAM_VALUE.CLK_VALUE_MHZ } {
-	# Procedure called to validate CLK_VALUE_MHZ
-	return true
-}
-
-proc update_PARAM_VALUE.REG_ADDRESS_WIDTH { PARAM_VALUE.REG_ADDRESS_WIDTH } {
-	# Procedure called to update REG_ADDRESS_WIDTH when any of the dependent parameters in the arguments change
-}
-
-proc validate_PARAM_VALUE.REG_ADDRESS_WIDTH { PARAM_VALUE.REG_ADDRESS_WIDTH } {
-	# Procedure called to validate REG_ADDRESS_WIDTH
-	return true
-}
-
-proc update_PARAM_VALUE.REG_SPACE_DEPTH { PARAM_VALUE.REG_SPACE_DEPTH } {
-	# Procedure called to update REG_SPACE_DEPTH when any of the dependent parameters in the arguments change
-}
-
-proc validate_PARAM_VALUE.REG_SPACE_DEPTH { PARAM_VALUE.REG_SPACE_DEPTH } {
-	# Procedure called to validate REG_SPACE_DEPTH
+proc validate_PARAM_VALUE.C_S00_AXI_ADDR_WIDTH { PARAM_VALUE.C_S00_AXI_ADDR_WIDTH } {
+	# Procedure called to validate C_S00_AXI_ADDR_WIDTH
 	return true
 }
 
@@ -68,28 +50,13 @@ proc validate_PARAM_VALUE.C_S00_AXI_HIGHADDR { PARAM_VALUE.C_S00_AXI_HIGHADDR } 
 }
 
 
-proc update_MODELPARAM_VALUE.CLK_VALUE_MHZ { MODELPARAM_VALUE.CLK_VALUE_MHZ PARAM_VALUE.CLK_VALUE_MHZ } {
+proc update_MODELPARAM_VALUE.C_S00_AXI_DATA_WIDTH { MODELPARAM_VALUE.C_S00_AXI_DATA_WIDTH PARAM_VALUE.C_S00_AXI_DATA_WIDTH } {
 	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
-	set_property value [get_property value ${PARAM_VALUE.CLK_VALUE_MHZ}] ${MODELPARAM_VALUE.CLK_VALUE_MHZ}
+	set_property value [get_property value ${PARAM_VALUE.C_S00_AXI_DATA_WIDTH}] ${MODELPARAM_VALUE.C_S00_AXI_DATA_WIDTH}
 }
 
-proc update_MODELPARAM_VALUE.BAUD_MAX_VALUE { MODELPARAM_VALUE.BAUD_MAX_VALUE PARAM_VALUE.BAUD_MAX_VALUE } {
+proc update_MODELPARAM_VALUE.C_S00_AXI_ADDR_WIDTH { MODELPARAM_VALUE.C_S00_AXI_ADDR_WIDTH PARAM_VALUE.C_S00_AXI_ADDR_WIDTH } {
 	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
-	set_property value [get_property value ${PARAM_VALUE.BAUD_MAX_VALUE}] ${MODELPARAM_VALUE.BAUD_MAX_VALUE}
-}
-
-proc update_MODELPARAM_VALUE.REG_ADDRESS_WIDTH { MODELPARAM_VALUE.REG_ADDRESS_WIDTH PARAM_VALUE.REG_ADDRESS_WIDTH } {
-	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
-	set_property value [get_property value ${PARAM_VALUE.REG_ADDRESS_WIDTH}] ${MODELPARAM_VALUE.REG_ADDRESS_WIDTH}
-}
-
-proc update_MODELPARAM_VALUE.AXI_DATA_WIDTH { MODELPARAM_VALUE.AXI_DATA_WIDTH PARAM_VALUE.AXI_DATA_WIDTH } {
-	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
-	set_property value [get_property value ${PARAM_VALUE.AXI_DATA_WIDTH}] ${MODELPARAM_VALUE.AXI_DATA_WIDTH}
-}
-
-proc update_MODELPARAM_VALUE.REG_SPACE_DEPTH { MODELPARAM_VALUE.REG_SPACE_DEPTH PARAM_VALUE.REG_SPACE_DEPTH } {
-	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
-	set_property value [get_property value ${PARAM_VALUE.REG_SPACE_DEPTH}] ${MODELPARAM_VALUE.REG_SPACE_DEPTH}
+	set_property value [get_property value ${PARAM_VALUE.C_S00_AXI_ADDR_WIDTH}] ${MODELPARAM_VALUE.C_S00_AXI_ADDR_WIDTH}
 }
 
--- axis_udp_filter_1.0/hdl/udp_filter.vh
+++
@@ -1,5 +0,0 @@
-`define MAC_WIDTH        48
-`define IP_V4_WIDTH      32
-`define UDP_WIDTH        16
-
-//`define DEBUG            1
