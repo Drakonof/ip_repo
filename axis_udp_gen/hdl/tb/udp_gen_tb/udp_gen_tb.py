d87a6c9c1824a9ce79840d8592f0baa84339eda5
Artem Shimko <artyom.shimko@gmail.com>
2023-11-15 20:23:59 +0400

    fix: taking data from init file

--- axis_udp_gen/hdl/tb/udp_gen_tb/udp_gen_tb.py
+++ axis_udp_gen/hdl/tb/udp_gen_tb/udp_gen_tb.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import random
+#todo: exaptions
 
 import cocotb
 from cocotb.clock import Clock
@@ -11,38 +11,56 @@ from cocotb.triggers import Timer
 class Tb(object):
     def __init__(self, dut):
         self.dut = dut
+        self.frame_array = []
+        
+        file = open('init.mem', 'r')
+        lines = file.readlines()
+        for line in lines:
+            value = int(line.strip(), 16)
+            self.frame_array.append(value)
+
+        self.frame_size = self.frame_array[0] + 2 # plus size and last value of frame
+        self.gap_size = 12
+
         cocotb.start_soon(Clock(self.dut.clk, 10, units="ns").start())
+
+
+    async def clk_tick(self):
+        await RisingEdge(self.dut.clk)
 
     async def reset(self):
         self.dut.s_rst_n.value = 0
         await RisingEdge(self.dut.clk)
         self.dut.s_rst_n.value = 1
 
-    async def set(self):
-        self.dut.dst_mac_addr.value = 0x444546474849
-        self.dut.src_ipv4_addr.value = 0x19216801
-        self.dut.dst_ipv4_addr.value = 0x19216802
-        self.dut.src_udp_port.value = 0x2134
-        self.dut.dst_udp_port.value = 0x5467
+    async def enable(self, en):
+        self.dut.en.value = en
 
-    async def start(self):
-        self.dut.en.value = 1
-        await RisingEdge(self.dut.clk)
+    async def send_frame(self):
+        for i in range(self.frame_size):
+            self.dut.mem_data.value = self.frame_array[self.dut.mem_addr.value]
+            await RisingEdge(self.dut.clk)
 
-    async def stop(self):
-        self.dut.en.value = 0
-        await RisingEdge(self.dut.clk)
-
-            
-
+        for i in range(self.gap_size):
+            self.dut.mem_data.value = self.frame_array[self.dut.mem_addr.value]
+            await RisingEdge(self.dut.clk)
+ 
 @cocotb.test()
 async def main(dut):
     cocotb.log.info("Start of simulation")
 
     tb = Tb(dut)
     await tb.reset()
-    await tb.set()
-    await tb.start()
+    await tb.enable(1)
+    await tb.send_frame()
+    await tb.send_frame()
+    await tb.enable(0)
+    await tb.clk_tick()
+    await tb.enable(1)
+    await tb.send_frame()
+    await tb.enable(0)
+
+
     await Timer(1000, units='ns')
 
 
