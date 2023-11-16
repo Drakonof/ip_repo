3cf5dc467f25871ebfc039ae118f3ca51ddbfe0a
Artem Shimko <artyom.shimko@gmail.com>
2023-11-16 17:34:53 +0400

    fix: mem depth

--- axis_udp_gen/hdl/rtl/axis_udp_gen.sv
+++ axis_udp_gen/hdl/rtl/axis_udp_gen.sv
@@ -2,18 +2,14 @@
 
 module axis_udp_gen #
 (
-  parameter unsigned                 AXIS_DATA_WIDTH = 64,
+  parameter unsigned AXIS_DATA_WIDTH = 64,
 
-  localparam unsigned AXI_DATA_WIDTH = AXIS_DATA_WIDTH, //todo 
-  parameter unsigned                 AXI_ADDR_WIDTH  = 4,
+  localparam unsigned MAX_FRAME_SIZE = 1518,
+  parameter unsigned MEM_DEPTH       = (MAX_FRAME_SIZE / AXIS_DATA_WIDTH) * 8,
 
-  localparam unsigned AXI_PROT_WIDTH = 3,
-  localparam unsigned AXI_RESP_WIDTH = 2
-
-  parameter                          INIT_FILE       = ""
+  parameter          INIT_FILE       = ""
 )
 (
-  // axis
   input  logic                                 axis_clk,
   input  logic                                 axis_s_rst_n,
   
@@ -21,47 +17,22 @@ module axis_udp_gen #
   output logic [AXIS_DATA_WIDTH - 1 : 0]       m_axis_tdata,
   output logic [(AXIS_DATA_WIDTH / 8) - 1 : 0] m_axis_tstrb,
   output logic                                 m_axis_tlast,
-  input  logic                                 m_axis_tready,
-  
-  // axi
-  // input  logic                                 axi_clk,
-  // input  logic                                 axi_s_rst_n,
-
-  // input  logic [AXI_ADDR_WIDTH - 1 : 0]        s_axi_awaddr,
-  // input  logic [AXI_PROT_WIDTH - 1 : 0]        s_axi_awprot,
-  // input  logic                                 s_axi_awvalid,
-  // output logic                                 s_axi_awready,
-  
-  // input  logic [AXI_DATA_WIDTH - 1 : 0]        s_axi_wdata,
-  // input  logic [(AXI_DATA_WIDTH / 8) - 1 : 0]  s_axi_wstrb,
-  // input  logic                                 s_axi_wvalid,
-  // output logic                                 s_axi_wready,
-  
-  // output logic [AXI_RESP_WIDTH - 1 : 0]        s_axi_bresp,
-  // output logic                                 s_axi_bvalid,
-  // input  logic                                 s_axi_bready,
-  
-  // input  logic [AXI_ADDR_WIDTH - 1 : 0]        s_axi_araddr,
-  // input  logic [AXI_PROT_WIDTH - 1 : 0]        s_axi_arprot,
-  // input  logic                                 s_axi_arvalid,
-  // output logic                                 s_axi_arready,
-  
-  // output logic [AXI_DATA_WIDTH - 1 : 0]        s_axi_rdata,
-  // output logic [AXI_RESP_WIDTH - 1 : 0]        s_axi_rresp,
-  // output logic                                 s_axi_rvalid,
-  // input  logic                                 s_axi_rready
+  input  logic                                 m_axis_tready
 );
 
 
-  logic en;
-  logic [AXIS_DATA_WIDTH - 1 : 0] data;
-  logic [AXIS_DATA_WIDTH - 1 : 0] mem_data;     
-  logic data_valid;
-  logic frame_end;
+  localparam unsigned MEM_ADDR_WIDTH = $clog2(MEM_DEPTH);
+  
 
-  logic [AXI_ADDR_WIDTH - 1 : 0]  mem_addr;
-  logic mem_rd_en;
+  logic                           en;
+
   //udp_gen_inf data_inf();
+  logic [AXIS_DATA_WIDTH - 1 : 0] data;
+  logic                           data_valid;
+  logic                           frame_end;
+
+  logic [AXIS_DATA_WIDTH - 1 : 0] mem_data; 
+  logic [MEM_ADDR_WIDTH - 1 : 0]  mem_addr;
 
 
   axis_udp_gen_ctrl # 
@@ -77,7 +48,8 @@ module axis_udp_gen #
   	.m_axis_tready (m_axis_tready),
   	
   	.en_o          (en           ),
-  	//.data_inf      (data_inf     )
+
+    //.data_inf      (data_inf     )
   	.data_i        (data     ),   
     .data_valid_i  (data_valid),
     .frame_end_i   (frame_end)
@@ -85,70 +57,30 @@ module axis_udp_gen #
   );
   
   udp_gen #
-  ( .DATA_WIDTH (AXI_DATA_WIDTH),
-    .ADDR_WIDTH (AXI_ADDR_WIDTH)
+  ( .DATA_WIDTH (AXIS_DATA_WIDTH),
+    .ADDR_WIDTH (MEM_ADDR_WIDTH )
   )
   udp_gen_inst_0
   (
     .clk_i          (axis_clk    ),
     .s_rst_n_i      (axis_s_rst_n),
   
-    .en_i           (en          )
+    .en_i           (en          ),
     
+    //.data_inf      (data_inf     )
     .data_o         (data        ),   
     .data_valid_o   (data_valid  ),
     .frame_end_o    (frame_end   ),
 
     .mem_data_i     (mem_data    ),
-    .mem_addr_o     (mem_addr    )//,
-   // .mem_rd_en_o    (mem_rd_en   )
+    .mem_addr_o     (mem_addr    )
 
   );
 
-  // axi_udp_gen_ctrl #
-  // (
-  //   .AXI_DATA_WIDTH (AXI_DATA_WIDTH),
-  //   .AXI_ADDR_WIDTH (AXI_ADDR_WIDTH)
-  // )
-  // axis_udp_gen_ctrl_inst_0
-  // (
-  //   .axi_clk        (axi_clk      ),
-  //   .axi_s_rst_n    (axi_s_rst_n  ),
-    
-  //   .s_axi_awaddr   (s_axi_awaddr ),
-  //   .s_axi_awprot   (s_axi_awprot ),
-  //   .s_axi_awvalid  (s_axi_awvalid),
-  //   .s_axi_awready  (s_axi_awready),
-    
-  //   .s_axi_wdata    (s_axi_wdata  ),
-  //   .s_axi_wstrb    (s_axi_wstrb  ),
-  //   .s_axi_wvalid   (s_axi_wvalid ),
-  //   .s_axi_wready   (s_axi_wready ),
-    
-  //   .s_axi_bresp    (s_axi_bresp  ),
-  //   .s_axi_bvalid   (s_axi_bvalid ),
-  //   .s_axi_bready   (s_axi_bready ),
-    
-  //   .s_axi_araddr   (s_axi_araddr ),
-  //   .s_axi_arprot   (s_axi_arprot ),
-  //   .s_axi_arvalid  (s_axi_arvalid),
-  //   .s_axi_arready  (s_axi_arready),
-    
-  //   .s_axi_rdata    (s_axi_rdata  ),
-  //   .s_axi_rresp    (s_axi_rresp  ),
-  //   .s_axi_rvalid   (s_axi_rvalid ),
-  //   .s_axi_rready   (s_axi_rready ),
-    
-  //   .bram_addr_o    (bram_addr    ),
-  //   .bram_data_o    (bram_data    ),
-  //   .bram_wr_en_o   (bram_wr_en   ),
-  //   .bram_byte_valid_o (bram_byte_valid)
-  // );
-
   rom #
   (
-    .DATA_WIDTH (AXI_DATA_WIDTH),
-    .ADDR_WIDTH (AXI_ADDR_WIDTH),
+    .DATA_WIDTH (AXIS_DATA_WIDTH),
+    .ADDR_WIDTH (MEM_ADDR_WIDTH ),
   
 `ifdef XILINX
     .RAM_TYPE   ("block"       ),
@@ -156,6 +88,7 @@ module axis_udp_gen #
 
     .INIT_FILE (INIT_FILE      )
   )
+  rom_inst_0
   (
     .clk_i  (axis_clk),
     .addr_i (mem_addr),
