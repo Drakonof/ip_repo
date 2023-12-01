ef7992beeb408548ed55de20d98c7640adb64143
Drakonof <artyom.shimko@gmail.com>
2023-12-01 14:25:28 +0400

    add: almost_empty signal for udp_filter

--- axis_udp_filter/hdl/rtl/axis_udp_filter.sv
+++ axis_udp_filter/hdl/rtl/axis_udp_filter.sv
@@ -31,7 +31,7 @@ module axis_udp_filter #
   output logic [AXIS_DATA_WIDTH - 1 : 0]       m_axis_tdata,
   output logic [(AXIS_DATA_WIDTH / 8) - 1 : 0] m_axis_tstrb,
   output logic                                 m_axis_tlast,
-  input  logic                                 m_axis_tready
+  input  logic                                 m_axis_tready,
   
   input  logic                                 s_axis_tvalid,
   input  logic [AXIS_DATA_WIDTH - 1 : 0]       s_axis_tdata,
@@ -39,13 +39,13 @@ module axis_udp_filter #
   input  logic                                 s_axis_tlast,
   output logic                                 s_axis_tready,
 
-  input  logic [IPV4_ADDR_WIDTH - 1 : 0]       ipv4_addr_i
+  input  logic [IPV4_ADDR_WIDTH - 1 : 0]       ipv4_addr
 );
   
 
   localparam unsigned FIFO_ADDR_WIDTH = $clog2(MAX_FRAME_SIZE);
   localparam unsigned ALMOST_FULL     = 2;
-  localparam unsigned ALMOST_EMPTY    = 2;
+  localparam unsigned ALMOST_EMPTY    = 1;
 
   logic                           fifo_rst_n;
   logic                           wr_en;
@@ -59,38 +59,38 @@ module axis_udp_filter #
   
   logic                           filter_en;
   logic [AXIS_DATA_WIDTH - 1 : 0] frame_data;
+  logic                           frame_data_valid;
   logic                           frame_last;
   logic                           frame_valid;
 
 
-  axis_udp_filter_if #
+  axis_udp_filter_if
+  axis_udp_filter_if_inst
   (
-    .AXIS_DATA_WIDTH (AXIS_DATA_WIDTH)
-  )
-  (
-    .m_axis_tvalid     (m_axis_tvalid ),
-    .m_axis_tdata      (m_axis_tdata  ),
-    .m_axis_tstrb      (m_axis_tstrb  ),
-    .m_axis_tlast      (m_axis_tlast  ),
-    .m_axis_tready     (m_axis_tready ),
+    .m_axis_tvalid      (m_axis_tvalid   ),
+    .m_axis_tdata       (m_axis_tdata    ),
+    .m_axis_tstrb       (m_axis_tstrb    ),
+    .m_axis_tlast       (m_axis_tlast    ),
+    .m_axis_tready      (m_axis_tready   ),
   
-    .s_axis_tvalid     (s_axis_tvalid ),
-    .s_axis_tdata      (s_axis_tdata  ),
-    .s_axis_tstrb      (s_axis_tstrb  ),
-    .s_axis_tlast      (s_axis_tlast  ),
-    .s_axis_tready     (s_axis_tready ),
+    .s_axis_tvalid      (s_axis_tvalid   ),
+    .s_axis_tdata       (s_axis_tdata    ),
+    .s_axis_tstrb       (s_axis_tstrb    ),
+    .s_axis_tlast       (s_axis_tlast    ),
+    .s_axis_tready      (s_axis_tready   ),
 
-    .en_i              (en            ),
+    .en_i               (en              ),
  
-    .fifo_full_i       (full          ),
-    .rd_en_o           (rd_en         ),
-    .data_from_fifo_i  (data_from_fifo),
-    .almost_empty_i    (almost_empty  ),
+    .fifo_full_i        (full            ),
+    .rd_en_o            (rd_en           ),
+    .data_from_fifo_i   (data_from_fifo  ),
+    .almost_empty_i     (almost_empty    ),
 
-    .filter_en_o       (filter_en     ),
-    .frame_o           (frame_data    ),
-    .frame_last_o      (frame_last    ),
-    .frame_valid_i     (frame_valid   )
+    .filter_en_o        (filter_en       ),
+    .frame_o            (frame_data      ),
+    .frame_data_valid_o (frame_data_valid),
+    .frame_last_o       (frame_last      ),
+    .frame_valid_i      (frame_valid     )
   );
 
   smart_fifo #
@@ -125,28 +125,27 @@ module axis_udp_filter #
     .empty_o        (empty         )
   );
 
-  udp_filter #
-  (
-    .DATA_WIDTH      (AXIS_DATA_WIDTH),
-
-    .IPV4_ADDR_WIDTH (IPV4_ADDR_WIDTH)
-  )
+  udp_filter
   udp_filter_inst
   (
-    .clk_i         (axis_clk    ),
+    .clk_i               (axis_clk        ),
 
-    .s_rst_n_i     (axis_s_rst_n),
-    .en_i          (filter_en   ),
+    .s_rst_n_i           (axis_s_rst_n    ),
+    .en_i                (filter_en       ),
 
-    .ipv4_addr_i   (ipv4_addr_i ),
+    .ipv4_addr_i         (ipv4_addr       ),
 
-    .frame_i       (frame       ),
-    .frame_last_i  (frame_last  ),
+    .frame_data_i        (frame_data      ),
+    .frame_last_i        (frame_last      ),
+    .frame_data_valid_i  (frame_data_valid),
 
-    .frame_valid_o (frame_valid ),
+    .frame_valid_o       (frame_valid     ),
   
-    .fifo_wr_en_o  (wr_en       ),
-    .fifo_data_o   (data_to_fifo),
-    .fifo_empty_i  (empty       ),
-    .fifo_rst_n_o  (fifo_rst_n  )
+    .fifo_wr_en_o        (wr_en           ),
+    .fifo_data_o         (data_to_fifo    ),
+    .fifo_almost_empty_i (almost_empty    ),
+    .fifo_empty_i        (empty           ),
+    .fifo_rst_n_o        (fifo_rst_n      )
   );
+
+endmodule
